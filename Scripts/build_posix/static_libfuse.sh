c0e6b0cba354325863e516a8bed61d451304c669
Hajin Jang <jb6804@naver.com>
2020-05-27 00:08:51 +0900

    Update wimlib to 1.13.2 (linux-x64)
    Rewrote wimlib build scripts

---
+++ Scripts/build_posix/static_libfuse.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+# static_lifuse.sh: compile static libxml
+
+# Check script arguments
+if [[ "$#" -ne 1 ]]; then
+    echo "Usage: $0 <LIBFUSE_SRCDIR>" >&2
+    exit 1
+fi
+if ![[ -d "$1" ]]; then
+    echo "[$1] is not a directory!" >&2
+    exit 1
+fi
+
+# Query environment info
+ARCH=$(uname -m) # x86_64, armv7l, aarch64, ...
+OS=$(uname -s) # Linux, Darwin, MINGW64_NT-10.0-18363, MSYS_NT-10.0-18363, ...
+
+# Set path and command vars
+# BASE_ABS_PATH: Absolute path of this script, e.g. /home/user/bin/foo.sh
+# BASE_DIR: Absolute path of the parent dir of this script, e.g. /home/user/bin
+if [ "${OS}" = Linux ]; then
+    BASE_ABS_PATH=$(readlink -f "$0")
+    CORES=$(grep -c ^processor /proc/cpuinfo)
+    DEST_LIB="libwim.so"
+    STRIP="strip"
+    CHECKDEP="ldd"
+elif [ "${OS}" = Darwin ]; then
+    BASE_ABS_PATH="$(cd $(dirname "$0");pwd)/$(basename "$0")"
+    CORES=$(sysctl -n hw.logicalcpu)
+    DEST_LIB="libwim.dylib"
+    STRIP="strip -x"
+    CHECKDEP="otool -L"
+else
+    echo "${OS} is not a supported platform!" >&2
+    exit 1
+fi
+BASE_DIR=$(dirname "${BASE_ABS_PATH}")
+LIB_PREFIX="${HOME}/wimlib-build"
+PKGCONF_DIR="${LIB_PREFIX}/lib/pkgconfig"
+
+# Create prefix directory
+mkdir "${LIB_PREFIX}"
+
+# Compile and copy wimlib binary
+# Adapted from https://wimlib.net/git/?p=wimlib;a=tree;f=tools/windeps/Makefile;
+pushd "${PWD}" > /dev/null
+cd $1
+rm -rf build
+mkdir build
+CFLAGS="-Os -fPIC" meson . build \
+    --strip --default-library static \
+    --prefix "${LIB_PREFIX}" \
+    --buildtype release \
+    -Dutils=false -Dexamples=false -Duseroot=false -Ddisable-mtab=false
+cd build
+ninja -j "${CORES}"
+ninja install
+popd > /dev/null
---
+++ Scripts/build_posix/static_wimlib.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+# static_wimlib.sh: compile shared wimlib linked with static libxml
+
+# Check script arguments
+if [[ "$#" -ne 1 ]]; then
+    echo "Usage: $0 <WIMLIB_SRCDIR>" >&2
+    exit 1
+fi
+if ![[ -d "$1" ]]; then
+    echo "[$1] is not a directory!" >&2
+    exit 1
+fi
+
+# Query environment info
+ARCH=$(uname -m) # x86_64, armv7l, aarch64, ...
+OS=$(uname -s) # Linux, Darwin, MINGW64_NT-10.0-18363, MSYS_NT-10.0-18363, ...
+
+# Set path and command vars
+# BASE_ABS_PATH: Absolute path of this script, e.g. /home/user/bin/foo.sh
+# BASE_DIR: Absolute path of the parent dir of this script, e.g. /home/user/bin
+if [ "${OS}" = Linux ]; then
+    BASE_ABS_PATH=$(readlink -f "$0")
+    CORES=$(grep -c ^processor /proc/cpuinfo)
+    DEST_LIB="libwim.so"
+    STRIP="strip"
+    CHECKDEP="ldd"
+elif [ "${OS}" = Darwin ]; then
+    BASE_ABS_PATH="$(cd $(dirname "$0");pwd)/$(basename "$0")"
+    CORES=$(sysctl -n hw.logicalcpu)
+    DEST_LIB="libwim.dylib"
+    STRIP="strip -x"
+    CHECKDEP="otool -L"
+else
+    echo "${OS} is not a supported platform!" >&2
+    exit 1
+fi
+BASE_DIR=$(dirname "${BASE_ABS_PATH}")
+LIB_PREFIX="${HOME}/wimlib-build"
+PKGCONF_DIR="${LIB_PREFIX}/lib/pkgconfig"
+
+# Check if libxml2 was properly compiled
+if ![[ -d "${LIB_PREFIX}" ]]; then
+    echo "Prefix directory [${LIB_PREFIX}] not found!" >&2
+    echo "Please run static_wimlib.sh first." >&2
+    exit 1
+fi
+if ![[ -d "${PKGCONF_DIR}" ]]; then
+    echo "PKGCONFIG directory [${PKGCONF_DIR}] not found!" >&2
+    echo "Please run static_wimlib.sh first. " >&2
+    exit 1
+fi
+if ![[ -s "${PKGCONF_DIR}/libxml-2.0.pc" ]]; then
+    echo "libxml2 not installed in [${LIB_PREFIX}]!" >&2
+    echo "Please run static_wimlib.sh first. " >&2
+    exit 1
+fi
+
+# Required dependencies
+# Debian/Ubuntu: sudo apt install libfuse-dev nasm
+# macOS:         brew install nasm
+which nasm
+if [[ $? -ne 0 ]]; then # Unable to find nasm
+    echo "Please install nasm!" >&2
+    if [ "${OS}" = Linux ]; then 
+        echo "Run \"sudo apt install nasm\"." >&2
+    elif [ "${OS}" = Darwin ]; then 
+        echo "Run \"brew install nasm\"." >&2
+    fi
+    exit 1
+fi
+
+# Preapre wimlib compiling
+# Turn on SSSE3 asm optimization on x64 build
+if [ "${ARCH}" = "x86_64" ]; then
+    EXTRA_ARGS="--enable-ssse3-sha1"
+fi
+# Turn off fuse on macOS build
+if [ "${OS}" = Darwin ]; then 
+    EXTRA_ARGS="${EXTRA_ARGS} --without-fuse"
+fi
+
+# Compile and copy wimlib binary
+# Adapted from https://wimlib.net/git/?p=wimlib;a=tree;f=tools/make-windows-release;
+pushd "${PWD}" > /dev/null
+cd $1
+make clean
+./configure --disable-static \
+    PKG_CONFIG_PATH="${PKGCONF_DIR}" \
+    --without-libcrypto --without-ntfs-3g "${EXTRA_ARGS}"
+make "-j${CORES}"
+cp ".libs/${DEST_LIB}" "${BASE_DIR}/${DEST_LIB}"
+popd > /dev/null
+#LIBXML2_CFLAGS="-I${LIB_PREFIX}/include/libxml2" \
+#LIBXML2_LIBS="-L${LIB_PREFIX}/lib -lxml2" \
+
+# Strip a binary
+ls -lh "${DEST_LIB}"
+${STRIP} "${DEST_LIB}"
+ls -lh "${DEST_LIB}"
+
+# Check dependency of a binary
+${CHECKDEP} "${DEST_LIB}"
