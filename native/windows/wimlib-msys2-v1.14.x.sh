8a0d7e32f2b232b670687ea3189c7607d439fe2a
Hajin Jang <jb6804@naver.com>
2025-02-08 01:13:58 +0900

    Update wimlib binaries to 1.14.4 (#9)
    * Update wimlib windows binaries to 1.14.4
    
    * Update wimlib macOS binaries to 1.14.4
    
    * Update wimlib binaries to 1.14.4
    
    * Update wimlib POSIX build script
    
    * Update linux wimlib build script
    
    * Update wimlib linux build script (2)
    
    * Build linux wimlib 1.14.4 without fuse3
    
    - libfuse3 is LGPL licensed, so it cannot be statically linked.
    - In Linux, shared library location is tricky to make it portable.
    - So disable it from packaged wimlib build.
    
    * Recompile wimlib 1.14.4 DLLs
    
    * Rewrite native windows compile script

--- native/windows/wimlib-msys2-v1.14.x.sh
+++ native/windows/wimlib-msys2-v1.14.x.sh
@@ -91,13 +91,13 @@ if [[ ! -z "${RADARE2_DIR}" ]]; then # -r not set
         RABIN2=
     fi
 fi
-if [[ -z "${RABIN2}" ]] ;then
+if [[ -z "${RABIN2}" ]]; then
     which rabin2 > /dev/null
-    if [[ $? -eq 0 ]]; then # rabin is callableUnable to find rabin2
+    if [[ $? -eq 0 ]]; then # rabin is callable
         RABIN2=rabin2
     fi
 fi
-if [[ ! -z "${RABIN2}" ]]; then
+if [[ ! -z "${RABIN2}" ]]; then # Unable to find rabin2
     CHECKDEP="${RABIN2} -Al"
 fi
 
@@ -115,27 +115,34 @@ if ! [[ -z "${TOOLCHAIN_DIR}" ]]; then
     export PATH=${TOOLCHAIN_DIR}/bin:${PATH}
 fi
 
+# Prevent libtool quirk which 'linker path does not have real file for...' error.
+# https://github.com/msys2/MINGW-packages/discussions/8056
+export lt_cv_deplibs_check_method=${lt_cv_deplibs_check_method='pass_all'}
+
 # Compile wimlib
 # Adapted from https://wimlib.net/git/?p=wimlib;a=tree;f=tools/make-windows-release;
 BUILD_MODES=( "exe" "lib" )
 pushd "${SRCDIR}" > /dev/null
 for BUILD_MODE in "${BUILD_MODES[@]}"; do
     CONFIGURE_ARGS=""
+    WIMLIB_CFLAGS="-D_NO_CRT_STDIO_INLINE"
     if [ "$BUILD_MODE" = "lib" ]; then
         CONFIGURE_ARGS="--disable-static --enable-shared"
         # WIMLIB_LDFLAGS="-no-undefined"
     elif [ "$BUILD_MODE" = "exe" ]; then
         CONFIGURE_ARGS="--enable-static --disable-shared"
     fi
-    
+
     make clean
     # ./configure --host=${TARGET_TRIPLE} --disable-static CFLAGS="-static-libgcc" \
     # --libdir=${SRCDIR} required for cross-compiling wimlib-imagex.exe.
     # If not, libtool automatically include `-L/ucrt64/lib`, causing x86_64 libmsvcrt.a/libmingw32.a to be always linked and cause an error.
-    ./configure --host=${TARGET_TRIPLE} --libdir="${SRCDIR}" \
+    # ./configure --host=${TARGET_TRIPLE} --libdir="${SRCDIR}" \
+    ./configure --host=${TARGET_TRIPLE} --libdir="${TOOLCHAIN_DIR}/${TARGET_TRIPLE}/lib" \
         ${CONFIGURE_ARGS} \
         --without-ntfs-3g --without-fuse \
-        "CFLAGS=${WIMLIB_CFLAGS}" "LDFLAGS=${WIMLIB_LDFLAGS}" \
+        CFLAGS="${WIMLIB_CFLAGS} -Os" \
+        LDFLAGS="${WIMLIB_LDFLAGS}" \
         ${EXTRA_ARGS}
     if [[ $? -ne 0 ]]; then # configure failed
         echo "./configure failed, please check config.log." >&2
@@ -155,6 +162,7 @@ popd > /dev/null
 pushd "${DEST_DIR}" > /dev/null
 echo
 echo "[*] Stripping [${DEST_LIB}]" 
+file *.dll *.exe
 ls -lh *.dll *.exe
 ${STRIP} *.dll *.exe
 ls -lh *.dll *.exe
